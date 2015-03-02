568e63231d770dcb015a327ca49df0c55636ad49
mitsa80 <dat11mta@student.lu.se>
2015-03-02 18:23:13 +0100

    har fått en random challenge

---
+++ php/get_challenge.php
@@ -0,0 +1,22 @@
+<?php
+
+//Nodebite black box
+include_once("../nodebite-swiss-army-oop.php");
+
+//create a new instance of the DBObjectSaver class 
+//and store it in the $ds variable
+	$ds = new DBObjectSaver(array(
+	  "host" => "127.0.0.1",
+	  "dbname" => "character_db",
+	  "username" => "root",
+	  "password" => "mysql",
+	  "prefix" => "DBOB"
+	));
+	
+	
+	if(isset($_REQUEST["ch"])){
+	
+		$index=(rand(0,9));
+		echo(json_encode($ds->challenges[$index]));
+	
+	}
